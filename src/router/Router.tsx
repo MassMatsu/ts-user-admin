24a51b537d0c9c3a17b5fbdeb02636adecfb15c5
MassMatsu <masa10matsukawa@gmail.com>
2021-03-18 13:30:04 +0900

    setup pages (Login, Home, UserAdmin, Setting, Page404) and routing

---
+++ src/router/Router.tsx
@@ -0,0 +1,33 @@
+import { memo, VFC } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import { Login } from '../components/pages/Login';
+import { Page404 } from '../components/pages/Page404';
+import { HomeRoutes } from './HomeRoutes';
+
+export const Router: VFC = memo(() => {
+  return (
+    <Switch>
+      <Route exact path='/'>
+        <Login />
+      </Route>
+      <Route
+        path='/home'
+        render={({ match: { url } }) => (
+          <Switch>
+            {HomeRoutes.map((route) => {
+              return (
+                <Route exact={route.exact} path={`${url}${route.path}`} key={route.path}>
+                  {route.children}
+                </Route>
+              );
+            })}
+          </Switch>
+        )}
+      ></Route>
+      <Route path='*'>
+        <Page404 />
+      </Route>
+    </Switch>
+  );
+});
