a6cb0130bc476dd056b684759d684c9e60f314a3
MassMatsu <masa10matsukawa@gmail.com>
2021-03-18 21:45:09 +0900

    set apperance for login page with chakra ui

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,28 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Divider,
+  Input,
+  Stack,
+} from '@chakra-ui/react';
 import { VFC, memo } from 'react';
 
+import {PrimaryButton} from '../atoms/button/PrimaryButton'
+
 export const Login: VFC = memo(() => {
-  return <p>it is Login page</p>;
+  return (
+    <Flex align='center' justify='center' height='100vh'>
+      <Box bg='white' w='sm' p={4} borderRadius='md' shadow='md'>
+        <Heading as='h1' size='lg' textAlign='center'>
+          User Admin App
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder='user ID' />
+          <PrimaryButton>login</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
